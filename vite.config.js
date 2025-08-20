f6344d38139b51a3215ef06732530e53d18bae67
Mogdad Abdelrahim <mogdad19998@gmail.com>
2025-08-20 13:16:28 +0400

    update Vite base path to match deployment structure

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/birthday-card-app/",
   plugins: [react(), tailwindcss()],
+  base: "/birthday-card/",
 });
